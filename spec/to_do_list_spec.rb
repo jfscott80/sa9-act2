084159161905cc82ecd976511ed5ff44c80a08ec
John Scott <jfscott@memphis.edu>
2024-02-22 10:20:07 -0600

    temperature converter spec file complete

--- spec/to_do_list_spec.rb
+++ spec/to_do_list_spec.rb
@@ -1,22 +1,22 @@
+# require "to_do_list"
 # RSpec.describe TodoList do
+#     let(:todo) { TodoList.new }
 #     describe "#add" do
 #       it "adds a todo to the list" do
-#         todolist.add(grocery)
-#         expect(grocery).to be in @todos
+#         expect(todo.add('grocery')).to be eq(todos[0])
 #       end
 #     end
   
 #     describe "#remove" do
 #       it "removes a todo from the list" do
-#         todolist.remove(grocery)
-#         expect(grocery).to not_be in @todos
+#         expect(todo.remove('grocery')).to not_be
 
 #       end
 #     end
   
 #     describe "#todos" do
 #       it "returns all todos" do
-#         expect(todolist.todos).to eq(@todos)
+#         expect(todo.todos).to be @todos
 #         # Fill in the test
 #       end
 #     end
